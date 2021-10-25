2df073bb3675ca6aec9500add13742720c6d7be8
Joseph March <josephmarch@gmail.com>
2021-10-24 21:01:54 -0400

    Updated with Object.values code take 3.

--- static/js/app.js
+++ static/js/app.js
@@ -5,24 +5,20 @@ var otu_labels = [];
 
 d3.json("samples.json").then(function(data) {
     console.log(data);
-    sample_values = Object.values(data.samples);
+    samples = Object.values(data.samples);
     console.log(sample_values);
-    otu_ids = Object.values(data.names);
-    console.log(otu_ids);
-    otu_labels = Object.values(data.metadata);
-    console.log(otu_labels);
 });
 
 // Horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual
-var bardata = [{
-    type: 'bar',
-    x: sample_values,
-    y: otu_ids,
-    orientation: 'h',
-    hoverinfo: "text",
-    hovertext: otu_labels
-  }];
+// var bardata = [{
+//     type: 'bar',
+//     x: sample_values,
+//     y: otu_ids,
+//     orientation: 'h',
+//     hoverinfo: "text",
+//     hovertext: otu_labels
+//   }];
   
-  Plotly.newPlot('myDiv', bardata);
+//   Plotly.newPlot('myDiv', bardata);
 
 
