9f997d15389b2af1f135b3f3d225670c0f0d85aa
Joseph March <josephmarch@gmail.com>
2021-10-24 22:14:40 -0400

    Update dropdown.

--- static/js/app.js
+++ static/js/app.js
@@ -3,7 +3,28 @@ d3.json("samples.json").then(function(data) {
     console.log(data);
     samples = Object.values(data.samples);
     console.log(samples);
+
+    // Select Dropdown
+    var dropdown = d3.select("#selDataset");
+
+    // Provide data to dorpdown
+    data.names.forEach(function(name){
+        dropdown.append("option").text(name);
+    });
+
+    // Select Demographic Info
+    var demographicInfo = d3.select("sample-metadata");
 });
+
+// On change to the Test Subject ID Number, call getData()
+// d3.selectAll("#selDataset").on("change", getData);
+
+// // Function called by Test Subject ID Number changes
+// function getData() {
+//     var dropdownMenu = d3.select("#selDataset");
+//     var dataset = dropdownMenu.property("value");
+    
+// }
 
 // Horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual
 // var bardata = [{
@@ -15,6 +36,6 @@ d3.json("samples.json").then(function(data) {
 //     hovertext: otu_labels
 //   }];
   
-//   Plotly.newPlot('myDiv', bardata);
+//   Plotly.newPlot("bar", bardata);
 
 
