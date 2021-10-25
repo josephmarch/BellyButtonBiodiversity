b7c76db58edca4ee085c25505b839da07304433f
Joseph March <josephmarch@gmail.com>
2021-10-25 02:43:58 -0400

    Demographics Data updated key-value pair take 3.

--- static/js/app.js
+++ static/js/app.js
@@ -9,22 +9,24 @@ function optionChanged(value) {
         metadata = Object.values(data.metadata.filter(function(metaid) {
             return metaid.id.toString() == value;
         }));
+        metadata = metadata[0];
 
         // Pull the samples for a particular id matching our value
         samples = Object.values(data.samples.filter(function(sample) {
             return sample.id.toString() == value;
         }));
+        samples = samples[0];
 
         // Sort the samples from largest to smallest
-        samples[0].sample_values.sort(function compareFunction(firstNum, secondNum) {
+        samples.sample_values.sort(function compareFunction(firstNum, secondNum) {
             // resulting order is (3, 2, 1)
             return secondNum - firstNum;
         });
 
         // Create slices of 10 values for the bar graph and reverse them
-        sample_values = samples[0].sample_values.slice(0, 10).reverse();
-        otu_ids = samples[0].otu_ids.slice(0, 10).reverse();
-        otu_labels = samples[0].otu_labels.slice(0, 10).reverse();
+        sample_values = samples.sample_values.slice(0, 10).reverse();
+        otu_ids = samples.otu_ids.slice(0, 10).reverse();
+        otu_labels = samples.otu_labels.slice(0, 10).reverse();
 
         // Horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual
         var bardata = [{
@@ -38,11 +40,9 @@ function optionChanged(value) {
         
           Plotly.newPlot("bar", bardata);
 
-        // Select Demographic Info and clear it, then fill it using key-value pairs
-        var demographicInfo = d3.select("sample-metadata");
-        demographicInfo.html("");
-        Object.entries(metadata[0]).forEach(([key, value]) => {
-            demographicInfo.append("p").text(`${key}: ${value}`);
+        // Select Demographic Info using key-value pairs
+        Object.entries(metadata).forEach(([key, value]) => {
+            d3.select("sample-metadata").append("p").text(`${key}: ${value}`);
         });
 
     });
