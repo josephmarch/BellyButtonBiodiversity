e320525c73fac4c09b633003fd1bcd9ff4e3af17
Joseph March <josephmarch@gmail.com>
2021-10-24 23:18:34 -0400

    Rearranged data for proper function placement.

--- static/js/app.js
+++ static/js/app.js
@@ -4,6 +4,18 @@ function optionChanged(value) {
     
     // Select Demographic Info
     //var demographicInfo = d3.select("sample-metadata");
+
+    // Horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual
+    // var bardata = [{
+    //     type: 'bar',
+    //     x: sample_values,
+    //     y: otu_ids,
+    //     orientation: 'h',
+    //     hoverinfo: "text",
+    //     hovertext: otu_labels
+    //   }];
+      
+    //   Plotly.newPlot("bar", bardata);
 }
 
 // Use the D3 library to read in samples.json and set up dropdown
@@ -28,25 +40,3 @@ d3.json("samples.json").then(function(data) {
     //d3.selectAll("#selDataset").on("change", optionChanged(this.value));
 
 });
-
-
-// Function called by Test Subject ID Number changes
-// function getData() {
-//     var dropdownMenu = d3.select("#selDataset");
-//     var dataset = dropdownMenu.property("value");
-    
-// }
-
-// Horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual
-// var bardata = [{
-//     type: 'bar',
-//     x: sample_values,
-//     y: otu_ids,
-//     orientation: 'h',
-//     hoverinfo: "text",
-//     hovertext: otu_labels
-//   }];
-  
-//   Plotly.newPlot("bar", bardata);
-
-
